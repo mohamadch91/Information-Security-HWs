3111d5c3b3c5a5e3f369953b63602f243657d30e
mohamadch91 <mohamadchoupan80@gmail.com>
2022-12-08 18:00:48 +0330

    feat: exception handling in Readfile

--- HW2/ReadFile.py
+++ HW2/ReadFile.py
@@ -4,18 +4,28 @@ def read_key() -> str:
     Returns:
         string: key to be used for encryption/decryption
     """
-    with open('key.txt', 'r') as f:
-        key :str = f.read()
-    return key
+    try:
+        with open('key.txt', 'r') as f:
+            key :str = f.read()
+            return key
+
+    except FileNotFoundError:
+        print("Key file not found")
+        exit()
 #Read cipher file from text file
 def read_cipher() -> str:
     """Read cipher file from text file
     Returns:
         string: cipher text to be decrypted
     """
-    with open('Cipher.txt', 'r') as f:
-        cipher: str = f.read()
-    return cipher
+    try:
+        with open('Cipher.txt', 'r') as f:
+            cipher: str = f.read()
+        return cipher
+        
+    except FileNotFoundError:
+        print("Cipher file not found")
+        exit()
 #Read input file from text file
 def read_input() -> str:
     """Read input file from text file
@@ -23,6 +33,10 @@ def read_input() -> str:
     Returns:
         string: input text to be encrypted
     """
-    with open('input.txt', 'r') as f:
-        input :str = f.read()
-    return input
+    try: 
+        with open('input.txt', 'r') as f:
+            input :str = f.read()
+        return input
+    except FileNotFoundError:
+        print("Input file not found")
+        exit()
