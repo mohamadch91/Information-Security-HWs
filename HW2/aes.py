3e764efa4e2612d959f929cc5330eb1e670abba9
mohamadch91 <mohamadchoupan80@gmail.com>
2022-12-08 18:50:24 +0330

    feat: initialize AES_CTR class

--- HW2/aes.py
+++ HW2/aes.py
@@ -1,4 +1,30 @@
 from typing import List
 from dataclasses import dataclass
+from os import urandom
 from .handleFile import *
 
+@dataclass
+class AES_CTR:
+    """AES_CTR class for encrypting and decrypting files using AES in counter mode
+    Attributes:
+        key: key for AES encryption
+        init_vector: initial vector for AES encryption
+    """
+    key: str = read_key()
+    initial_vector : List[int] = [0] * 16
+
+    def create_salt(self) -> bytes:
+        """Create a random salt for the key
+        Returns:
+            random salt
+        """
+        return urandom(16)
+
+    def convert_key_256 (self ) -> None:
+        """Convert the key to 256 bits
+        """
+        self.key = self.key + self.create_salt()
+        
+
+
+    
