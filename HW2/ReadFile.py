b7dfbe7f3be870b7943d68ff00d55703c3a1a585
mohamadch91 <mohamadchoupan80@gmail.com>
2022-12-08 17:52:19 +0330

    feat: variable types add

--- HW2/ReadFile.py
+++ HW2/ReadFile.py
@@ -1,28 +1,28 @@
 #Read key file from text file
-def read_key():
+def read_key() -> str:
     """Read key file from text file
     Returns:
         string: key to be used for encryption/decryption
     """
     with open('key.txt', 'r') as f:
-        key = f.read()
+        key :str = f.read()
     return key
 #Read cipher file from text file
-def read_cipher():
+def read_cipher() -> str:
     """Read cipher file from text file
     Returns:
         string: cipher text to be decrypted
     """
     with open('Cipher.txt', 'r') as f:
-        cipher = f.read()
+        cipher: str = f.read()
     return cipher
 #Read input file from text file
-def read_input():
+def read_input() -> str:
     """Read input file from text file
 
     Returns:
         string: input text to be encrypted
     """
     with open('input.txt', 'r') as f:
-        input = f.read()
+        input :str = f.read()
     return input
