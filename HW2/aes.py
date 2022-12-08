0be09794b1b954ad191fa17d2d7bc222efd88051
mohamadch91 <mohamadchoupan80@gmail.com>
2022-12-08 21:40:48 +0330

    feat: colors to project

--- HW2/aes.py
+++ HW2/aes.py
@@ -6,6 +6,17 @@ import pbkdf2
 import binascii
 import secrets
 import pyaes
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 @dataclass
 class AES_CTR:
     """AES_CTR class for encrypting and decrypting files using AES in counter mode
